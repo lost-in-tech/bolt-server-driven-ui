e0ed08436fce8695bd2c459f68d825d9451e304c
ruhul <ruhul.amin@carsales.com.au>
2024-03-12 19:13:40 +1100

    wip..

---
+++ src/SampleApi/Features/Home/Hero/SectionProvider.cs
@@ -0,0 +1,21 @@
+﻿using Bolt.IocScanner.Attributes;
+using Bolt.MaySucceed;
+using Bolt.ServerDrivenUI;
+using Bolt.ServerDrivenUI.Core;
+using Bolt.ServerDrivenUI.Core.Elements;
+using Bolt.ServerDrivenUI.Extensions.Web.RazorParser;
+
+namespace SampleApi.Features.Home.Hero;
+
+[AutoBind]
+internal sealed class SectionProvider(IRazorXmlViewParser parser) : ScreenSectionProvider<HomePageRequest>
+{
+    public override string ForSection => "hero";
+
+    public override Task<MaySucceed<IElement>> Get(IRequestContextReader context, HomePageRequest request, CancellationToken ct)
+    {
+        var rsp = parser.Read<SectionProvider>(new ());
+
+        return rsp;
+    }
+}
---
+++ src/SampleApi/Features/Home/Hero/Views/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SampleApi.Features.Home.Hero.Views;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
