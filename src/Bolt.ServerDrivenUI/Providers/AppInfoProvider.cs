15c335719213b6e736e39c14f2f6df999890a7ec
mruhul <mruhul@gmail.com>
2024-06-03 21:54:48 +1000

    WIP

--- src/Bolt.ServerDrivenUI/Providers/AppInfoProvider.cs
+++ src/Bolt.ServerDrivenUI/Providers/AppInfoProvider.cs
@@ -5,7 +5,7 @@ namespace Bolt.ServerDrivenUI.Providers;
 
 internal sealed class AppInfoProvider : IAppInfoProvider
 {
-    private static readonly Lazy<AppInfo> Instance = new Lazy<AppInfo>(() => new AppInfo
+    private static readonly Lazy<AppInfo> Instance = new(() => new AppInfo
     {
         Name = AppDomain.CurrentDomain.FriendlyName.Replace(".", "-").ToLowerInvariant(),
         Version = Assembly.GetEntryAssembly()?.GetName().Version ?? new Version(1, 0, 0)
